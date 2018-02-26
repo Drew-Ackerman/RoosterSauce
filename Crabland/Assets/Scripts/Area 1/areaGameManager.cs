28f8360a18bae8fca6d4b124f2aff53cc2936797
bhoward4450 <bradleyhoward@mail.weber.edu>
2018-02-26 13:50:21 -0700

    Stripped down project
    Removed a lot of unnecessary components from the project. Also added touch-responsive player movement controls.

---
+++ Crabland/Assets/Scripts/Area 1/areaGameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class areaGameManager : MonoBehaviour {
+	private GameObject btnOkay;
+	private Text livesText;
+
+	// Use this for initialization
+	void Start () {
+		//set intial player conditions, to be placed in function
+
+		//get UI components
+		btnOkay = GameObject.Find("btnOkay");
+		btnOkay.SetActive(false);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Crabland/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch(0);
+			//make sure either 
+			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+				// If the finger is on the screen, move the object smoothly to the touch position
+				Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));                
+				transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
+			}
+		}
+	}
+}
