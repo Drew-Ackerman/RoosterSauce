68e60d947d565340511b9b339393f0bf4b56bb12
SabreeGoff <sabreegoff@mail.weber.edu>
2018-02-26 09:47:22 -0700

    English to thai translation
    basically notes. English to thai translation.

---
+++ EngToThai.swift
@@ -0,0 +1,26 @@
+Dialects
+orientation
+16x9
+tablets: android galaxy s3, samsung j7v
+thai translation
+tablet model
+
+
+
+Go to the house to find watergun
+
+
+Find a water source to refil water watergun
+
+
+pick up watergun
+
+
+you are running low on water
+
+
+watergun is already full
+
+
+
+
