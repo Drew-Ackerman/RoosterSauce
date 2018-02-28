7b05ffbf7b0bf3977974433a59edc1879b00b19d
bhoward4450 <bradleyhoward@mail.weber.edu>
2018-02-28 08:39:54 -0700

    Added Dialogue Box Generator

---
+++ Crabland/Assets/Scripts/Area 1/areaTreasureTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class areaTreasureTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.name == "Player"){
+			loadDialogue loadDialogue = GameObject.Find("UIScripts").GetComponent<loadDialogue>();
+			loadDialogue.showDialogue();
+		}
+	}
+}
---
+++ Crabland/Assets/Scripts/loadDialogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//call this function with 2 gameobjects to be shown in a dialogue view
+public class loadDialogue : MonoBehaviour {
+	public GameObject left, right;
+
+	public void showDialogue(){
+		GameObject canvas = GameObject.Find("Canvas");
+		GameObject dialogue = (GameObject)Instantiate(Resources.Load("UITextBox"));
+		dialogue.transform.SetParent(canvas.transform);
+		dialogue.GetComponent<RectTransform>().localPosition = new Vector3(0, -220, 0);
+	}
+
+}
