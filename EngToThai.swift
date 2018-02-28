43bce96bc8dc8ae597766720009b213d6c42f9be
SabreeGoff <sabreegoff@mail.weber.edu>
2018-02-28 09:52:48 -0700

    Meeting notes.
    Very unorganized notes from the meeting.

--- EngToThai.swift
+++ EngToThai.swift
@@ -1,25 +1,45 @@
-Dialects
-orientation
-16x9
-tablets: android galaxy s3, samsung j7v
-thai translation
-tablet model
 
 
+Thai/English vocabulary LAYOUT
 
-Go to the house to find watergun
+    ENG: Go to the house to find watergun
+    ENG: Find a water source to refil water watergun
+    ENG: pick up watergun
+    ENG: you are running low on water
+    ENG: watergun is already full
+
+------------------------------------------------------------------------------------------------------------
+
+NOTES FROM MEETING 1:
+    Dialects: This will be taken care of later, use google translate for now
+    orientation: doesnt matter. We will most likely do horizontal 16x9
+    tablets: android galaxy s3, samsung j7v
+    tablet model: latest android version
+
+    HIS LAYOUT:
+        play story 4 times.
+            vocab builder
+            story
+            fun activity
+        first round
+            90% thai
+        last round
+            100% english
+        fifth round should be a fun game
+
+    Our layout is different but he approves.
 
 
-Find a water source to refil water watergun
+    A few ideas, nothing concrete
+        * click on word and hear it out loud
+        * click on object to hear english
+        * switch between thai and english
+        * convert one word in sentence
+        * thai kid reads sentence, each word has to be approved after at most three tries.
 
+    Useful tools:
+        android speech 3$
 
-pick up watergun
-
-
-you are running low on water
-
-
-watergun is already full
 
 
 
