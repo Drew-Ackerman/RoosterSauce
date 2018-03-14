376046fa7a40c8d7dc3e664412e92e83245d10ea
bhoward4450 <bradleyhoward@mail.weber.edu>
2018-03-14 13:17:09 -0600

    Added toggleable text objects
    Text can now be clicked to be toggled between Thai and English. Sentences are bilingualText objects loaded into arrays and displayed.

--- Crabland/Assets/Scripts/PlayerController.cs
+++ Crabland/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,11 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public bool moveActive = true;
 
-	private Rigidbody2D rb2d;
+	//private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start() {
-		rb2d = GetComponent<Rigidbody2D>();
+		//rb2d = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
