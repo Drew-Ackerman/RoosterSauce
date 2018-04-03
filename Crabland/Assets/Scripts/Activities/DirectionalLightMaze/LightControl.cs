4baece725f36f6efe78f36768bd19973b594f438
bhoward4450 <bradleyhoward@mail.weber.edu>
2018-04-03 14:53:39 -0600

    Added ActDirectionalLightMaze starter files
    Direcitonal Light Maze works, light dynamically adjusts based on wall distance from player. Voice control still to be implemented.

---
+++ Crabland/Assets/Scripts/Activities/DirectionalLightMaze/LightControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+	//layer specified to detect light range
+	public LayerMask layer;
+	//update direction based on spoken word (UP, RIGHT, DOWN, LEFT)
+	public Vector2 facingDirection = new Vector2(0, -1);
+	public int maxLightDistance = 10;
+
+	//'flashlight' used by player
+	private Light light;
+
+	void Start(){
+		light = gameObject.GetComponent<Light>();
+	}
+
+	void Update(){
+		//send out ray in facing direction
+		RaycastHit2D hit = Physics2D.Raycast(transform.parent.position, facingDirection, Mathf.Infinity, layer);
+		if(hit.collider) {
+			//find distance to hit object
+			if(hit.distance + 3 < maxLightDistance) //light only extends a certain range
+				light.range = hit.distance + 3; //shine light to that distance + small buffer
+			else
+				light.range = maxLightDistance;
+		}
+	}
+}
--- Crabland/Assets/Scripts/Area 1/areaGameManager.cs
+++ Crabland/Assets/Scripts/Area 1/areaGameManager.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 //script to be used to control the flow and state of game events
 //will handle closing text when appropriate, keeping track of current event progress
 public class areaGameManager : MonoBehaviour {
-	GameObject btnOkay;
-
 	// Use this for initialization
 	void Start () {
 		
