f4acba4664b6065b923b59198a13e952624b9944
bhoward4450 <bradleyhoward@mail.weber.edu>
2018-04-11 12:56:21 -0600

    Glossary Starter

--- Crabland/Assets/Scripts/Activities/CompleteSentence/CompleteSentenceManager.cs
+++ Crabland/Assets/Scripts/Activities/CompleteSentence/CompleteSentenceManager.cs
@@ -7,6 +7,8 @@ public class CompleteSentenceManager : MonoBehaviour {
 	public string item1, item2, item3;
 	public GameObject selected;
 
+	private int questionNum = 1;
+
 	//player must finish the sentence with the word of the item to be grabbed.
 	//ex 'Uncle, I found the [watermelon, the hat, and the trumpet]'
 	//receive items from main forest item selection
@@ -16,12 +18,6 @@ public class CompleteSentenceManager : MonoBehaviour {
 		item2 = "Trumpet";
 		item3 = "Hat";
 		TTSManager.Initialize(transform.name, "OnTTSInit");
-
-		TTSManager.Speak("Uncle", false, TTSManager.STREAM.Music, 1f, 0f, transform.name);
-		TTSManager.Speak("I", false, TTSManager.STREAM.Music, 1f, 0f, transform.name);
-		TTSManager.Speak("Found", false, TTSManager.STREAM.Music, 1f, 0f, transform.name);
-		TTSManager.Speak("The", false, TTSManager.STREAM.Music, 1f, 0f, transform.name);
-		TTSManager.Speak("Apple", false, TTSManager.STREAM.Music, 1f, 0f, transform.name);
 
 		GameObject ButtonsHead = GameObject.Find("SelectionItemButtons");
 		Button b1 = ButtonsHead.transform.GetChild(0).GetComponent<Button>();
@@ -50,7 +46,58 @@ public class CompleteSentenceManager : MonoBehaviour {
 
 	//pass the string to this function to read the item out loud
 	public void speakItem(string itemToSpeak) {
-		TTSManager.Speak(itemToSpeak, false, TTSManager.STREAM.Music, 1f, 0f, transform.name);
+		TTSManager.Speak(itemToSpeak, true, TTSManager.STREAM.Music, 1f, 0f, transform.name);
+		if(itemToSpeak == item1 || itemToSpeak == item2 || itemToSpeak == item3) {		
+			if (questionNum == 1){
+
+				GameObject ButtonsHead = GameObject.Find("SelectionItemButtons");
+				Button b1 = ButtonsHead.transform.GetChild(0).GetComponent<Button>();
+				b1.GetComponentInChildren<Text>().text = "Water";
+				b1.onClick.RemoveAllListeners();
+				b1.onClick.AddListener(delegate {speakItem(b1.GetComponentInChildren<Text>().text);});
+
+				Button b2 = ButtonsHead.transform.GetChild(1).GetComponent<Button>();
+				b2.GetComponentInChildren<Text>().text = "Trumpet";
+				b2.onClick.RemoveAllListeners();
+				b2.onClick.AddListener(delegate {speakItem(b2.GetComponentInChildren<Text>().text);});
+
+				Button b3 = ButtonsHead.transform.GetChild(2).GetComponent<Button>();
+				b3.GetComponentInChildren<Text>().text = "Crayon";
+				b3.onClick.RemoveAllListeners();
+				b3.onClick.AddListener(delegate {speakItem(b3.GetComponentInChildren<Text>().text);});
+
+				Button b4 = ButtonsHead.transform.GetChild(3).GetComponent<Button>();
+				b4.GetComponentInChildren<Text>().text = "Salad";
+				b4.onClick.RemoveAllListeners();
+				b4.onClick.AddListener(delegate {speakItem(b4.GetComponentInChildren<Text>().text);});
+
+				questionNum++;
+			} else if (questionNum == 2){
+
+				GameObject ButtonsHead = GameObject.Find("SelectionItemButtons");
+				Button b1 = ButtonsHead.transform.GetChild(0).GetComponent<Button>();
+				b1.GetComponentInChildren<Text>().text = "Chair";
+				b1.onClick.RemoveAllListeners();
+				b1.onClick.AddListener(delegate {speakItem(b1.GetComponentInChildren<Text>().text);});
+
+				Button b2 = ButtonsHead.transform.GetChild(1).GetComponent<Button>();
+				b2.GetComponentInChildren<Text>().text = "Smile";
+				b2.onClick.RemoveAllListeners();
+				b2.onClick.AddListener(delegate {speakItem(b2.GetComponentInChildren<Text>().text);});
+
+				Button b3 = ButtonsHead.transform.GetChild(2).GetComponent<Button>();
+				b3.GetComponentInChildren<Text>().text = "Hat";
+				b3.onClick.RemoveAllListeners();
+				b3.onClick.AddListener(delegate {speakItem(b3.GetComponentInChildren<Text>().text);});
+
+				Button b4 = ButtonsHead.transform.GetChild(3).GetComponent<Button>();
+				b4.GetComponentInChildren<Text>().text = "Top";
+				b4.onClick.RemoveAllListeners();
+				b4.onClick.AddListener(delegate {speakItem(b4.GetComponentInChildren<Text>().text);});
+
+				questionNum++;
+			}
+		}
 	}
 		
 	void OnTTSInit(string message) {
