7805fb843b172af614052d937516576d2e04aca1
Drew Ackerman <mitcha12@live.com>
2018-04-16 11:15:07 -0600

    Main Menu Scene and Manager edited.

--- Crabland/Assets/Scripts/Main Menu/mainMenuManager.cs
+++ Crabland/Assets/Scripts/Main Menu/mainMenuManager.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainMenuManager : MonoBehaviour {
-	private GameObject crab, crabBlue;
-	private float time1, time2;
 
 	void Start(){
-		time1 = Time.time;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//while below view area
-		time2 = Time.time;
-		if(time2 - time1 > 4) {
-			int rand = Random.Range(0, 10);
-			if(rand > 8) {
-			} else {
-				time1 = Time.time;
-			}
-		}
-
-		
+	
 	}
 
 	public void closeGame(){
@@ -31,6 +19,6 @@ public class mainMenuManager : MonoBehaviour {
 	}
 
 	public void startGame(){
-		SceneManager.LoadScene ("Area1", LoadSceneMode.Single);
+		SceneManager.LoadScene ("Forest", LoadSceneMode.Single);
 	}
 }
